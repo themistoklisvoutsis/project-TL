3098363a94ea479ebcde408bf2c9e0d9a668340d
VasileiouGeorge <geopavasil@gmail.com>
2025-05-20 23:45:11 +0300

    .

--- ProgramSelectionv2/gymratingdbversion/src/SearchResultsScreen.java
+++
@@ -1,6 +0,0 @@
-public class SearchResultsScreen {
-    public void gymProfileSelect(String gymName) {
-        GymProfileScreen gymProfileScreen = new GymProfileScreen(gymName);
-        gymProfileScreen.showGymProfileScreen();
-    }
-}
