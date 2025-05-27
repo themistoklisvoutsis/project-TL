7ccbe06064f6fa170123bcbaa65044bd08245d95
giorgosspan <geoduke2004@outlook.com>
2025-05-27 22:25:34 +0300

    personal progress code

---
+++ personalprogress/src/ProgressComparisonScreen.java
@@ -0,0 +1,7 @@
+public class ProgressComparisonScreen {
+    public void progressComparisonAndDisplay(String results, int maxWorkOuts) {
+        System.out.println("Comparison Results:");
+        System.out.println("Results: " + results);
+        System.out.println("Maximum Workouts: " + maxWorkOuts);
+    }
+}
