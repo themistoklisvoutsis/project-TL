6703fa3c2057bd3400f4466697afe39fca14b6d5
themistoklisvoutsis <tvoutsis@gmail.com>
2025-05-29 23:18:50 +0300

    update of a class

--- GymNet/src/main/java/NotTakingPartScreen.java
+++ GymNet/src/main/java/NotTakingPartScreen.java
@@ -1,2 +1,8 @@
 public class NotTakingPartScreen {
+
+    public static void display()
+    {
+        System.out.println("Unfortunately you cannot upload your comment because you have never been enrolled with this gym.");
+    }
+
 }
